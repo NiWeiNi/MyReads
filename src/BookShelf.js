83f8e7eac8cf9190f2172f3de058be6893e61de2
Wei <weinini007@gmail.com>
2018-06-10 15:11:26 +0200

    refactor: fix book with no thumbnail

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -11,7 +11,7 @@ class BookShelf extends Component {
                         <li key={book.id}>
                             <div className="book">
                                 <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks ? `url(${book.imageLinks.thumbnail})`: null }}></div>
                                 <div className="book-shelf-changer">
                                     <select defaultValue = {this.props.shelf} onClick={(event) => this.props.onChangeShelf(event, book)}>
                                         <option value="none" disabled>Move to...</option>
--- src/BooksSearch.js
+++ src/BooksSearch.js
@@ -54,7 +54,7 @@ class BooksSearch extends Component {
                         <li key={book.id}>
                             <div className="book">
                                 <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks ? `url(${book.imageLinks.thumbnail})`: null }}></div>
                                 <div className="book-shelf-changer">
                                     <select defaultValue = {book.shelf} onClick={(event) => this.props.onChangeShelf(event, book)}>
                                         <option value="none" disabled>Move to...</option>
