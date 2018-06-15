c9b3e0a4163a1505525c3fd6d19577ffe3d14214
Wei <weinini007@gmail.com>
2018-06-15 22:50:22 +0200

    refactor: updated bookShelf with no book component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -13,7 +13,24 @@ class BookShelf extends Component {
                 <div className="bookshelf-books">
                     <ol className="books-grid">
                         {this.props.books.filter((book) => book.shelf === `${this.props.shelf}`).map((book) => (
-                            <Book book={this.props.books} /> 
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks ? `url(${book.imageLinks.thumbnail})`: null }}></div>
+                                    <div className="book-shelf-changer">
+                                        <select defaultValue = {this.props.shelf} onClick={(event) => this.props.onChangeShelf(event, book)}>
+                                            <option value="none" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                        </select>
+                                    </div>
+                                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">{book.authors}</div>
+                                </div>
+                            </li>   
                         ))}
                     </ol>
                 </div>
