707e0e5586ba0e9e1a2eb25dd1ec076f33200799
Wei <weinini007@gmail.com>
2018-06-17 23:22:37 +0200

    reafactor: change bookShelf to stateless functional component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,42 +1,40 @@
 import React, {Component} from 'react'
 import sortBy from 'sort-by'
 
-class BookShelf extends Component {
+const BookShelf = ({books, title, shelf, onChangeShelf}) => {
     
-    render() {
+    return (
         // Sort books by title
-        this.props.books.sort(sortBy('title'))
-
-        return(
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.title}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {this.props.books.filter((book) => book.shelf === `${this.props.shelf}`).map((book) => (
-                            <li key={book.id}>
-                                <div className="book">
-                                    <div className="book-top">
-                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks ? `url(${book.imageLinks.thumbnail})`: null }}></div>
-                                    <div className="book-shelf-changer">
-                                        <select defaultValue = {this.props.shelf} onClick={(event) => this.props.onChangeShelf(event, book)}>
-                                            <option value="noneSelected" disabled>Move to...</option>
-                                            <option value="currentlyReading">Currently Reading</option>
-                                            <option value="wantToRead">Want to Read</option>
-                                            <option value="read">Read</option>
-                                            <option value="none">None</option>
-                                        </select>
-                                    </div>
-                                    </div>
-                                    <div className="book-title">{book.title}</div>
-                                    <div className="book-authors">{book.authors}</div>
+        books.sort(sortBy('title')),
+        // Content of the shelves, book and selector
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{title}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books.filter((book) => book.shelf === `${shelf}`).map((book) => (
+                        <li key={book.id}>
+                            <div className="book">
+                                <div className="book-top">
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks ? `url(${book.imageLinks.thumbnail})`: null }}></div>
+                                <div className="book-shelf-changer">
+                                    <select defaultValue = {shelf} onClick={(event) => onChangeShelf(event, book)}>
+                                        <option value="noneSelected" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
                                 </div>
-                            </li>   
-                        ))}
-                    </ol>
-                </div>
+                                </div>
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">{book.authors}</div>
+                            </div>
+                        </li>   
+                    ))}
+                </ol>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default BookShelf
